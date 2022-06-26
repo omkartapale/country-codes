6dd041245c0caa50e9589974ad087bbd966ab350
Omkar Tapale <omkartapale@gmail.com>
2022-06-26 20:26:37 +0530

    Fix: scss files mislinks

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,5 @@
 import logo from '../assets/logo.svg';
-import '../styles/App.scss';
+import '../scss/App.scss';
 
 function App() {
   return (
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './styles/index.scss';
+import './scss/index.scss';
 import App from './components/App';
 import reportWebVitals from './reportWebVitals';
 

9ff34906bb0efa2bfc61449f073abd9593ab94a3
Omkar Tapale <omkartapale@gmail.com>
2022-07-02 13:10:06 +0530

    Added App Icon in header

--- src/components/AppTitle.js
+++ src/components/AppTitle.js
@@ -3,6 +3,7 @@ import "../scss/AppTitle.scss";
 function AppTitle() {
   return (
     <div className="AppTitle">
+      <img src="./apple-touch-icon.png" alt="worldmap" height="94" />
       <h1 className="my-3 display-4">Country Code Lookup</h1>
     </div>
   );

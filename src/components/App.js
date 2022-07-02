f52e642153c69953cc0a1f10d9a643a710cfeda7
Omkar Tapale <omkartapale@gmail.com>
2022-07-02 14:37:10 +0530

    Added Country Details UI

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,14 @@
 import "../scss/App.scss";
 import Header from "../components/Header";
 import Search from "./Search";
+import CountryDetails from "./CountryDetails";
 
 function App() {
   return (
     <div className="App">
       <Header></Header>
       <Search></Search>
+      <CountryDetails></CountryDetails>
     </div>
   );
 }
---
+++ src/components/CountryDetails.js
@@ -0,0 +1,97 @@
+import "../scss/CountryDetails.scss";
+
+const CountryDetails = () => {
+  return (
+    <div className="CountryDetails container">
+      <div className="row mt-5 pb-3 justify-content-center">
+        <div className="col-md-10">
+          <div class="card px-4 pt-4 pb-3">
+            <div class="card-body">
+              <h2 className="mb-4">Found India for you!</h2>
+              <div className="row">
+              <div className="col-md-6 col-xl-3">
+                  <p className="mb-3">Country Name:</p>
+                  <p className="display-6 d-flex align-items-center justify-content-between">
+                    India
+                    <button className="btn btn-sm">
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        width="24"
+                        height="24"
+                        fill="currentColor"
+                        class="bi bi-clipboard2"
+                        viewBox="0 0 16 16"
+                      >
+                        <path d="M3.5 2a.5.5 0 0 0-.5.5v12a.5.5 0 0 0 .5.5h9a.5.5 0 0 0 .5-.5v-12a.5.5 0 0 0-.5-.5H12a.5.5 0 0 1 0-1h.5A1.5 1.5 0 0 1 14 2.5v12a1.5 1.5 0 0 1-1.5 1.5h-9A1.5 1.5 0 0 1 2 14.5v-12A1.5 1.5 0 0 1 3.5 1H4a.5.5 0 0 1 0 1h-.5Z" />
+                        <path d="M10 .5a.5.5 0 0 0-.5-.5h-3a.5.5 0 0 0-.5.5.5.5 0 0 1-.5.5.5.5 0 0 0-.5.5V2a.5.5 0 0 0 .5.5h5A.5.5 0 0 0 11 2v-.5a.5.5 0 0 0-.5-.5.5.5 0 0 1-.5-.5Z" />
+                      </svg>
+                    </button>
+                  </p>
+                </div>
+                <div className="col-md-6 col-xl-3">
+                  <p className="mb-3">Alpha-2 Code:</p>{" "}
+                  <p className="display-6 d-flex align-items-center justify-content-between">
+                    IN
+                    <button className="btn btn-sm">
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        width="24"
+                        height="24"
+                        fill="currentColor"
+                        class="bi bi-clipboard2"
+                        viewBox="0 0 16 16"
+                      >
+                        <path d="M3.5 2a.5.5 0 0 0-.5.5v12a.5.5 0 0 0 .5.5h9a.5.5 0 0 0 .5-.5v-12a.5.5 0 0 0-.5-.5H12a.5.5 0 0 1 0-1h.5A1.5 1.5 0 0 1 14 2.5v12a1.5 1.5 0 0 1-1.5 1.5h-9A1.5 1.5 0 0 1 2 14.5v-12A1.5 1.5 0 0 1 3.5 1H4a.5.5 0 0 1 0 1h-.5Z" />
+                        <path d="M10 .5a.5.5 0 0 0-.5-.5h-3a.5.5 0 0 0-.5.5.5.5 0 0 1-.5.5.5.5 0 0 0-.5.5V2a.5.5 0 0 0 .5.5h5A.5.5 0 0 0 11 2v-.5a.5.5 0 0 0-.5-.5.5.5 0 0 1-.5-.5Z" />
+                      </svg>
+                    </button>
+                  </p>
+                </div>
+                <div className="col-md-6 col-xl-3">
+                  <p className="mb-3">Alpha-3 Code:</p>{" "}
+                  <p className="display-6 d-flex align-items-center justify-content-between">
+                    IND
+                    <button className="btn btn-sm">
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        width="24"
+                        height="24"
+                        fill="currentColor"
+                        class="bi bi-clipboard2"
+                        viewBox="0 0 16 16"
+                      >
+                        <path d="M3.5 2a.5.5 0 0 0-.5.5v12a.5.5 0 0 0 .5.5h9a.5.5 0 0 0 .5-.5v-12a.5.5 0 0 0-.5-.5H12a.5.5 0 0 1 0-1h.5A1.5 1.5 0 0 1 14 2.5v12a1.5 1.5 0 0 1-1.5 1.5h-9A1.5 1.5 0 0 1 2 14.5v-12A1.5 1.5 0 0 1 3.5 1H4a.5.5 0 0 1 0 1h-.5Z" />
+                        <path d="M10 .5a.5.5 0 0 0-.5-.5h-3a.5.5 0 0 0-.5.5.5.5 0 0 1-.5.5.5.5 0 0 0-.5.5V2a.5.5 0 0 0 .5.5h5A.5.5 0 0 0 11 2v-.5a.5.5 0 0 0-.5-.5.5.5 0 0 1-.5-.5Z" />
+                      </svg>
+                    </button>
+                  </p>
+                </div>
+                <div className="col-md-6 col-xl-3">
+                  <p className="mb-3">Num-3 Code:</p>
+                  <p className="display-6 d-flex align-items-center justify-content-between">
+                    246
+                    <button className="btn btn-sm">
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        width="24"
+                        height="24"
+                        fill="currentColor"
+                        class="bi bi-clipboard2"
+                        viewBox="0 0 16 16"
+                      >
+                        <path d="M3.5 2a.5.5 0 0 0-.5.5v12a.5.5 0 0 0 .5.5h9a.5.5 0 0 0 .5-.5v-12a.5.5 0 0 0-.5-.5H12a.5.5 0 0 1 0-1h.5A1.5 1.5 0 0 1 14 2.5v12a1.5 1.5 0 0 1-1.5 1.5h-9A1.5 1.5 0 0 1 2 14.5v-12A1.5 1.5 0 0 1 3.5 1H4a.5.5 0 0 1 0 1h-.5Z" />
+                        <path d="M10 .5a.5.5 0 0 0-.5-.5h-3a.5.5 0 0 0-.5.5.5.5 0 0 1-.5.5.5.5 0 0 0-.5.5V2a.5.5 0 0 0 .5.5h5A.5.5 0 0 0 11 2v-.5a.5.5 0 0 0-.5-.5.5.5 0 0 1-.5-.5Z" />
+                      </svg>
+                    </button>
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default CountryDetails;

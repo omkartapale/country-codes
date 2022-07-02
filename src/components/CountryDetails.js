68633453c4781e17f53f169bac17635a5984d47c
Omkar Tapale <omkartapale@gmail.com>
2022-07-02 20:39:27 +0530

    Added shadow to country details card Added svg for clipboard buttons Updated styles for clipboard buttons

--- src/components/CountryDetails.js
+++ src/components/CountryDetails.js
@@ -1,30 +1,26 @@
 import "../scss/CountryDetails.scss";
+import { ReactComponent as ClipboardSvg } from "../assets/clipboard.svg";
+import { ReactComponent as CopiedSvg } from "../assets/check.svg";
 
 const CountryDetails = () => {
   return (
     <div className="CountryDetails container">
       <div className="row mt-4 mt-md-5 pb-3 justify-content-center">
         <div className="col-md-10">
-          <div class="card px-4 pt-4 pb-3">
+          <div class="card px-4 pt-4 pb-3 shadow-sm">
             <div class="card-body">
               <h2 className="mb-4">Found India for you!</h2>
               <div className="row">
-              <div className="col-md-6 col-xl-3">
+                <div className="col-md-6 col-xl-3">
                   <p className="mb-3">Country Name:</p>
                   <p className="display-6 d-flex align-items-center justify-content-between">
                     India
-                    <button className="btn btn-sm">
-                      <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        width="24"
-                        height="24"
-                        fill="currentColor"
-                        class="bi bi-clipboard2"
-                        viewBox="0 0 16 16"
-                      >
-                        <path d="M3.5 2a.5.5 0 0 0-.5.5v12a.5.5 0 0 0 .5.5h9a.5.5 0 0 0 .5-.5v-12a.5.5 0 0 0-.5-.5H12a.5.5 0 0 1 0-1h.5A1.5 1.5 0 0 1 14 2.5v12a1.5 1.5 0 0 1-1.5 1.5h-9A1.5 1.5 0 0 1 2 14.5v-12A1.5 1.5 0 0 1 3.5 1H4a.5.5 0 0 1 0 1h-.5Z" />
-                        <path d="M10 .5a.5.5 0 0 0-.5-.5h-3a.5.5 0 0 0-.5.5.5.5 0 0 1-.5.5.5.5 0 0 0-.5.5V2a.5.5 0 0 0 .5.5h5A.5.5 0 0 0 11 2v-.5a.5.5 0 0 0-.5-.5.5.5 0 0 1-.5-.5Z" />
-                      </svg>
+                    <button
+                      className="btn-clipboard"
+                      type="button"
+                      title="Copy to clipboard"
+                    >
+                      <ClipboardSvg width="24" height="24" />
                     </button>
                   </p>
                 </div>
@@ -32,18 +28,12 @@ const CountryDetails = () => {
                   <p className="mb-3">Alpha-2 Code:</p>{" "}
                   <p className="display-6 d-flex align-items-center justify-content-between">
                     IN
-                    <button className="btn btn-sm">
-                      <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        width="24"
-                        height="24"
-                        fill="currentColor"
-                        class="bi bi-clipboard2"
-                        viewBox="0 0 16 16"
-                      >
-                        <path d="M3.5 2a.5.5 0 0 0-.5.5v12a.5.5 0 0 0 .5.5h9a.5.5 0 0 0 .5-.5v-12a.5.5 0 0 0-.5-.5H12a.5.5 0 0 1 0-1h.5A1.5 1.5 0 0 1 14 2.5v12a1.5 1.5 0 0 1-1.5 1.5h-9A1.5 1.5 0 0 1 2 14.5v-12A1.5 1.5 0 0 1 3.5 1H4a.5.5 0 0 1 0 1h-.5Z" />
-                        <path d="M10 .5a.5.5 0 0 0-.5-.5h-3a.5.5 0 0 0-.5.5.5.5 0 0 1-.5.5.5.5 0 0 0-.5.5V2a.5.5 0 0 0 .5.5h5A.5.5 0 0 0 11 2v-.5a.5.5 0 0 0-.5-.5.5.5 0 0 1-.5-.5Z" />
-                      </svg>
+                    <button
+                      className="btn-clipboard"
+                      type="button"
+                      title="Copied"
+                    >
+                      <ClipboardSvg width="24" height="24" />
                     </button>
                   </p>
                 </div>
@@ -51,18 +41,12 @@ const CountryDetails = () => {
                   <p className="mb-3">Alpha-3 Code:</p>{" "}
                   <p className="display-6 d-flex align-items-center justify-content-between">
                     IND
-                    <button className="btn btn-sm">
-                      <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        width="24"
-                        height="24"
-                        fill="currentColor"
-                        class="bi bi-clipboard2"
-                        viewBox="0 0 16 16"
-                      >
-                        <path d="M3.5 2a.5.5 0 0 0-.5.5v12a.5.5 0 0 0 .5.5h9a.5.5 0 0 0 .5-.5v-12a.5.5 0 0 0-.5-.5H12a.5.5 0 0 1 0-1h.5A1.5 1.5 0 0 1 14 2.5v12a1.5 1.5 0 0 1-1.5 1.5h-9A1.5 1.5 0 0 1 2 14.5v-12A1.5 1.5 0 0 1 3.5 1H4a.5.5 0 0 1 0 1h-.5Z" />
-                        <path d="M10 .5a.5.5 0 0 0-.5-.5h-3a.5.5 0 0 0-.5.5.5.5 0 0 1-.5.5.5.5 0 0 0-.5.5V2a.5.5 0 0 0 .5.5h5A.5.5 0 0 0 11 2v-.5a.5.5 0 0 0-.5-.5.5.5 0 0 1-.5-.5Z" />
-                      </svg>
+                    <button
+                      className="btn-clipboard"
+                      type="button"
+                      title="Copied"
+                    >
+                      <CopiedSvg width="24" height="24" />
                     </button>
                   </p>
                 </div>
@@ -70,18 +54,12 @@ const CountryDetails = () => {
                   <p className="mb-3">Num-3 Code:</p>
                   <p className="display-6 d-flex align-items-center justify-content-between">
                     246
-                    <button className="btn btn-sm">
-                      <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        width="24"
-                        height="24"
-                        fill="currentColor"
-                        class="bi bi-clipboard2"
-                        viewBox="0 0 16 16"
-                      >
-                        <path d="M3.5 2a.5.5 0 0 0-.5.5v12a.5.5 0 0 0 .5.5h9a.5.5 0 0 0 .5-.5v-12a.5.5 0 0 0-.5-.5H12a.5.5 0 0 1 0-1h.5A1.5 1.5 0 0 1 14 2.5v12a1.5 1.5 0 0 1-1.5 1.5h-9A1.5 1.5 0 0 1 2 14.5v-12A1.5 1.5 0 0 1 3.5 1H4a.5.5 0 0 1 0 1h-.5Z" />
-                        <path d="M10 .5a.5.5 0 0 0-.5-.5h-3a.5.5 0 0 0-.5.5.5.5 0 0 1-.5.5.5.5 0 0 0-.5.5V2a.5.5 0 0 0 .5.5h5A.5.5 0 0 0 11 2v-.5a.5.5 0 0 0-.5-.5.5.5 0 0 1-.5-.5Z" />
-                      </svg>
+                    <button
+                      className="btn-clipboard"
+                      type="button"
+                      title="Copy to clipboard"
+                    >
+                      <ClipboardSvg width="24" height="24" />
                     </button>
                   </p>
                 </div>

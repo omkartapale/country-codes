4f9751ce86211ac957e363fe1f8f7a7657af9c4e
Omkar Tapale <omkartapale@gmail.com>
2022-07-03 16:56:13 +0530

    Additional way to load image from public directory

--- src/components/AppTitle.js
+++ src/components/AppTitle.js
@@ -4,6 +4,7 @@ import logo from "../assets/worldmap.png";
 function AppTitle() {
   return (
     <div className="AppTitle">
+      {/* <img src={process.env.PUBLIC_URL + '/apple-touch-icon.png'} alt="worldmap" height="94" /> */}
       <img src={logo} alt="worldmap" height="94" />
       <h1 className="my-3 display-4">Country Code Lookup</h1>
     </div>

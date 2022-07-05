07609bac562dc815c48a332700ddd27903c870ed
Omkar Tapale <omkartapale@gmail.com>
2022-07-05 21:54:26 +0530

    Added Help page component

---
+++ src/pages/Help.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+export class Help extends Component {
+  render() {
+    return (
+      <div className="Help">
+        This is help page.
+      </div>
+    );
+  }
+}
+
+export default Help;

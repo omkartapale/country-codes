5427ab4d8cf2fdb3997f192491c348def5072388
Omkar Tapale <omkartapale@gmail.com>
2022-07-07 14:04:52 +0530

    Added Convert page

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./scss/App.scss";
 import Header from "./components/common/Header";
 import Home from "./pages/Home";
+import Convert from "./pages/Convert";
 import Help from "./pages/Help";
 
 function App() {
@@ -11,8 +12,9 @@ function App() {
       <Router>
         <Header />
         <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="help" element={<Help />} />
+          <Route exact path="/" element={<Home />} />
+          <Route exact path="convert" element={<Convert />} />
+          <Route exact path="help" element={<Help />} />
         </Routes>
       </Router>
     </div>
---
+++ src/pages/Convert.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+export class Convert extends Component {
+  render() {
+    return (
+      <div className="Convert">
+        This is Convert page.
+      </div>
+    );
+  }
+}
+
+export default Convert;

b1f60c32e59eed11f3135074993a3c21f14e21e5
Omkar Tapale <omkartapale@gmail.com>
2022-07-09 21:57:37 +0530

    Adapted changes from previous commit

--- src/components/CountryDetails.js
+++ src/components/CountryDetails.js
@@ -3,7 +3,7 @@ import CopyButton from "./common/CopyButton";
 
 const CountryDetails = ({ countryFound }) => {
   return (
-    <div className="CountryDetails container">
+    <div className="CountryDetails">
       <div className="row mt-4 mt-md-5 pb-3 justify-content-center">
         <div className="col-md-10">
           <div className="card px-4 pt-4 pb-3 shadow-sm">

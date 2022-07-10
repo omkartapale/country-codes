c6d9273c48ac222b5acfab8a631d3570c20eb34d
Omkar Tapale <omkartapale@gmail.com>
2022-07-10 19:15:52 +0530

    Replace relevant tab while interconvert url changed - Added props for InterconvertCodes and InterconvertNavTabs to fetch conversionType

--- src/App.js
+++ src/App.js
@@ -18,18 +18,18 @@ function App() {
           <Route path="home" element={<Home />} />
           <Route path="interconvert" element={<Interconvert />}>
             <Route index element={<InterconvertMenu />} />
-            <Route path="country-to-alpha2" element={<InterconvertCodes />} />
-            <Route path="country-to-alpha3" element={<InterconvertCodes />} />
-            <Route path="country-to-num3" element={<InterconvertCodes />} />
-            <Route path="alpha2-to-country" element={<InterconvertCodes />} />
-            <Route path="alpha2-to-alpha3" element={<InterconvertCodes />} />
-            <Route path="alpha2-to-num3" element={<InterconvertCodes />} />
-            <Route path="alpha3-to-country" element={<InterconvertCodes />} />
-            <Route path="alpha3-to-alpha2" element={<InterconvertCodes />} />
-            <Route path="alpha3-to-num3" element={<InterconvertCodes />} />
-            <Route path="num3-to-country" element={<InterconvertCodes />} />
-            <Route path="num3-to-alpha2" element={<InterconvertCodes />} />
-            <Route path="num3-to-alpha3" element={<InterconvertCodes />} />
+            <Route path="country-to-alpha2" element={<InterconvertCodes conversionType="ctoa2"/>} />
+            <Route path="country-to-alpha3" element={<InterconvertCodes conversionType="ctoa3"/>} />
+            <Route path="country-to-num3" element={<InterconvertCodes conversionType="cton3"/>} />
+            <Route path="alpha2-to-country" element={<InterconvertCodes conversionType="a2toc"/>} />
+            <Route path="alpha2-to-alpha3" element={<InterconvertCodes conversionType="a2toa3"/>} />
+            <Route path="alpha2-to-num3" element={<InterconvertCodes conversionType="a2ton3"/>} />
+            <Route path="alpha3-to-country" element={<InterconvertCodes conversionType="a3toc"/>} />
+            <Route path="alpha3-to-alpha2" element={<InterconvertCodes conversionType="a3toa2"/>} />
+            <Route path="alpha3-to-num3" element={<InterconvertCodes conversionType="a3ton3"/>} />
+            <Route path="num3-to-country" element={<InterconvertCodes conversionType="n3toc"/>} />
+            <Route path="num3-to-alpha2" element={<InterconvertCodes conversionType="n3toa2"/>} />
+            <Route path="num3-to-alpha3" element={<InterconvertCodes conversionType="n3toa3"/>} />
           </Route>
           <Route path="help" element={<Help />} />
         </Routes>
--- src/components/InterconvertNavTabs.js
+++ src/components/InterconvertNavTabs.js
@@ -1,33 +1,71 @@
+import PropTypes from "prop-types";
 import React, { Component } from "react";
 import { NavLink } from "react-router-dom";
 import { ReactComponent as InterconvertSvg } from "../assets/arrow-left-right.svg";
 
 export class InterconvertNavTabs extends Component {
+  static propTypes = {
+    conversionType: PropTypes.string.isRequired,
+  };
+
   render() {
-    // const pathname = window.location.pathname;
-    // todo check pathname for current route and display replacement tabs for existing one
-    // if route is Alpha2 to Country then replace Country to Alpha2 and show active
     return (
       <div className="InterconvertNavTabs">
         <nav className="nav nav-tabs nav-justified">
-          <NavLink to="/interconvert/country-to-alpha2" className="nav-link">
-            Country <InterconvertSvg /> Alpha-2
-          </NavLink>
-          <NavLink to="/interconvert/country-to-alpha3" className="nav-link">
-            Country <InterconvertSvg /> Alpha-3
-          </NavLink>
-          <NavLink to="/interconvert/country-to-num3" className="nav-link">
-            Country <InterconvertSvg /> Num-3
-          </NavLink>
-          <NavLink to="/interconvert/alpha2-to-alpha3" className="nav-link">
-            Alpha-2 <InterconvertSvg /> Alpha-3
-          </NavLink>
-          <NavLink to="/interconvert/alpha2-to-num3" className="nav-link">
-            Alpha-2 <InterconvertSvg /> Num-3
-          </NavLink>
-          <NavLink to="/interconvert/alpha3-to-num3" className="nav-link">
-            Alpha-3 <InterconvertSvg /> Num-3
-          </NavLink>
+          {this.props.conversionType === "a2toc" ? (
+            <NavLink to="/interconvert/alpha2-to-country" className="nav-link">
+              Alpha-2 <InterconvertSvg /> Country
+            </NavLink>
+          ) : (
+            <NavLink to="/interconvert/country-to-alpha2" className="nav-link">
+              Country <InterconvertSvg /> Alpha-2
+            </NavLink>
+          )}
+          {this.props.conversionType === "a3toc" ? (
+            <NavLink to="/interconvert/alpha3-to-country" className="nav-link">
+              Alpha-3 <InterconvertSvg /> Country
+            </NavLink>
+          ) : (
+            <NavLink to="/interconvert/country-to-alpha3" className="nav-link">
+              Country <InterconvertSvg /> Alpha-3
+            </NavLink>
+          )}
+          {this.props.conversionType === "n3toc" ? (
+            <NavLink to="/interconvert/num3-to-country" className="nav-link">
+              Num-3 <InterconvertSvg /> Country
+            </NavLink>
+          ) : (
+            <NavLink to="/interconvert/country-to-num3" className="nav-link">
+              Country <InterconvertSvg /> Num-3
+            </NavLink>
+          )}
+          {this.props.conversionType === "a3toa2" ? (
+            <NavLink to="/interconvert/alpha3-to-alpha2" className="nav-link">
+              Alpha-3 <InterconvertSvg /> Alpha-2
+            </NavLink>
+          ) : (
+            <NavLink to="/interconvert/alpha2-to-alpha3" className="nav-link">
+              Alpha-2 <InterconvertSvg /> Alpha-3
+            </NavLink>
+          )}
+          {this.props.conversionType === "n3toa2" ? (
+            <NavLink to="/interconvert/num3-to-alpha2" className="nav-link">
+              Num-2 <InterconvertSvg /> Alpha-2
+            </NavLink>
+          ) : (
+            <NavLink to="/interconvert/alpha2-to-num3" className="nav-link">
+              Alpha-2 <InterconvertSvg /> Num-3
+            </NavLink>
+          )}
+          {this.props.conversionType === "n3toa3" ? (
+            <NavLink to="/interconvert/num3-to-alpha3" className="nav-link">
+              Num-3 <InterconvertSvg /> Alpha-3
+            </NavLink>
+          ) : (
+            <NavLink to="/interconvert/alpha3-to-num3" className="nav-link">
+              Alpha-3 <InterconvertSvg /> Num-3
+            </NavLink>
+          )}
         </nav>
       </div>
     );
